ae40d5663a7ce2cbb6c05453211ebb546ec49b57
Ikhsan Mochammad Noor <31455796+ikhsanmn@users.noreply.github.com>
2021-02-23 21:40:48 +0700

    add js for gamma and index
    gamma map dor create data "must add function simulation" and index for view of better

---
+++ simulation samarium on reactor/fungsigammap.js
@@ -0,0 +1,26 @@
+promethium();
+function promethium(){
+	var r;
+	var t;
+
+	var gammap=1.13; //uranium 235 //uranium 233 0.66 //1.9 239Pu 
+	 //per hour
+
+	var flux;
+	var dis_flux_neutron = [flux];
+	var i;
+	for (i=0;i<1000000; i++) {
+		i+=1
+		dis_flux_neutron[i] ;
+
+	}
+
+	console.log(dis_flux_neutron);
+}
+
+prometium2();
+function promethium2(){
+	var lammdap=0.0128;
+
+	var 
+}
